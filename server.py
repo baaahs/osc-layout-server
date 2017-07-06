80bc22b34e8b57fd8387b87cc16e232e95cd1745
Josh Durbin <durbinjo593@gmail.com>
2017-07-06 15:55:50 -0700

    updated server, adding caching, support for concurrent clients
    updated server, adding caching, support for concurrent clients such that the server will transmit, for each request, each available layout until all layouts are served within the span of one minute, then clear. if additional requests are made for layouts within a minute where all layouts have been served the server responds with a 404

--- server.py
+++ server.py
@@ -1,27 +1,49 @@
-
 import cherrypy
 import os
 import zipfile
-import itertools
+from cachetools import TTLCache
 
 class OSCLayoutServer(object):
 
     osc_zipfile = 'index.xml'
     layouts_directory = 'layouts'
+    ttl_cache_timeout_in_seconds = 60
+    cache_max_entries = 30
+    cache = TTLCache(maxsize=cache_max_entries, ttl=ttl_cache_timeout_in_seconds)
 
     def __init__(self):
         _layouts_path = os.path.join(os.path.dirname(__file__), self.layouts_directory)
-        self._layouts = itertools.cycle(os.listdir(_layouts_path))
+        self.layouts = os.listdir(_layouts_path)
 
     @cherrypy.expose
     def index(self):
 
-        _layout = self._layouts.next()
+        client_ip = cherrypy.request.remote.ip
+
+        if self.cache.__contains__(client_ip):
+
+            served_client_layouts = self.cache[client_ip]
+            yet_to_be_served_client_layouts = list(set(self.layouts).difference(set(served_client_layouts)))
+
+            if yet_to_be_served_client_layouts:
+
+                layout_to_serve = yet_to_be_served_client_layouts[0]
+                served_client_layouts.append(layout_to_serve)
+
+            else:
+
+                raise cherrypy.HTTPError(404, "Client has received all layouts")
+
+        else:
+
+            layout_to_serve = self.layouts[0]
+
+            self.cache[client_ip] = [layout_to_serve]
 
         cherrypy.response.headers['Content-type'] = 'application/touchosc'
-        cherrypy.response.headers['Content-Disposition'] = 'attachment; filename="' + _layout + '"'
+        cherrypy.response.headers['Content-Disposition'] = 'attachment; filename="' + layout_to_serve + '"'
 
-        _osc_layout_file_fullpath = os.path.join(os.path.dirname(__file__), self.layouts_directory, _layout)
-        _osc_layout_zipfile = zipfile.ZipFile(_osc_layout_file_fullpath)
+        osc_layout_file_fullpath = os.path.join(os.path.dirname(__file__), self.layouts_directory, layout_to_serve)
+        osc_layout_zipfile = zipfile.ZipFile(osc_layout_file_fullpath)
 
-        return _osc_layout_zipfile.open(self.osc_zipfile, 'r')
+        return osc_layout_zipfile.open(self.osc_zipfile, 'r')
