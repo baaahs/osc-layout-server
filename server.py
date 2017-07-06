e5a18ff7afc71d0f2b12bf7e2be48a127345e397
Josh Durbin <durbinjo593@gmail.com>
2017-07-06 16:16:20 -0700

    optimizations. done.

--- server.py
+++ server.py
@@ -8,12 +8,12 @@ class OSCLayoutServer(object):
     osc_zipfile = 'index.xml'
     layouts_directory = 'layouts'
     ttl_cache_timeout_in_seconds = 60
-    cache_max_entries = 30
+    cache_max_entries = 20
     cache = TTLCache(maxsize=cache_max_entries, ttl=ttl_cache_timeout_in_seconds)
 
     def __init__(self):
         _layouts_path = os.path.join(os.path.dirname(__file__), self.layouts_directory)
-        self.layouts = os.listdir(_layouts_path)
+        self.layouts = set(os.listdir(_layouts_path))
 
     @cherrypy.expose
     def index(self):
@@ -22,13 +22,13 @@ class OSCLayoutServer(object):
 
         if self.cache.__contains__(client_ip):
 
-            served_client_layouts = self.cache[client_ip]
-            yet_to_be_served_client_layouts = list(set(self.layouts).difference(set(served_client_layouts)))
+            transmitted_layouts = self.cache[client_ip]
+            pending_transmission_layouts = self.layouts.difference(transmitted_layouts)
 
-            if yet_to_be_served_client_layouts:
+            if pending_transmission_layouts:
 
-                layout_to_serve = yet_to_be_served_client_layouts[0]
-                served_client_layouts.append(layout_to_serve)
+                layout_to_transmit = pending_transmission_layouts.pop()
+                transmitted_layouts.add(layout_to_transmit)
 
             else:
 
@@ -36,14 +36,14 @@ class OSCLayoutServer(object):
 
         else:
 
-            layout_to_serve = self.layouts[0]
+            layout_to_transmit = self.layouts.copy().pop()
 
-            self.cache[client_ip] = [layout_to_serve]
+            self.cache[client_ip] = {layout_to_transmit}
 
         cherrypy.response.headers['Content-type'] = 'application/touchosc'
-        cherrypy.response.headers['Content-Disposition'] = 'attachment; filename="' + layout_to_serve + '"'
+        cherrypy.response.headers['Content-Disposition'] = 'attachment; filename="' + layout_to_transmit + '"'
 
-        osc_layout_file_fullpath = os.path.join(os.path.dirname(__file__), self.layouts_directory, layout_to_serve)
-        osc_layout_zipfile = zipfile.ZipFile(osc_layout_file_fullpath)
+        osc_layout_file_full_path = os.path.join(os.path.dirname(__file__), self.layouts_directory, layout_to_transmit)
+        osc_layout_zipfile = zipfile.ZipFile(osc_layout_file_full_path)
 
         return osc_layout_zipfile.open(self.osc_zipfile, 'r')
